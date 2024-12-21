55e33f01508b56606e454cb960fc7ede2ce9526a
Azamshaik7 <azamshari68@gmail.com>
2024-12-21 13:45:39 +0530

    dd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,63 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import './navbar.css';
+// import logo from '../images/logo1.jpg';
+
+// export default function Navbar() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <nav className={`navbarMain ${isOpen ? 'open' : ''}`}>
+//       <div className="navbar">
+//         <div className="logomain">
+//           <Link to="/">
+//           <img className="logo" src={logo} alt="Physio Works Logo" />
+//           </Link> 
+//           <p>Great Indian Physiotherapist</p>
+//         </div>
+//         <div className="hamburger" onClick={toggleMenu}>
+//           ☰
+//         </div>
+//         <ul className="ulList">
+//           <li className='navlist' ><Link to="/" className='activelist'>Home</Link></li> 
+//           <li className='navlist'>Online Yoga</li>
+//           <li className='navlist'>
+//             <Link to='/services' className='activelist'>Services</Link>
+//           </li>
+//           <li className='navlist'>Healing Stories</li>
+//           <li className='navlist'><Link to="/sign-in" className='activelist'>SignIn</Link></li>
+//           <li>
+//             <Link className='adjust' to="/book-appointment">
+//               <button className="btn22">Book an Appointment</button>
+//             </Link> 
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
+
+
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 import logo from '../images/logo1.jpg';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Check if the token exists in localStorage
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true); // User is logged in
+    }
+  }, []); // Empty dependency array means this runs once on component mount
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -15,25 +68,37 @@ export default function Navbar() {
       <div className="navbar">
         <div className="logomain">
           <Link to="/">
-          <img className="logo" src={logo} alt="Physio Works Logo" />
-          </Link> 
+            <img className="logo" src={logo} alt="Physio Works Logo" />
+          </Link>
           <p>Great Indian Physiotherapist</p>
         </div>
         <div className="hamburger" onClick={toggleMenu}>
           ☰
         </div>
         <ul className="ulList">
-          <li className='navlist' ><Link to="/" className='activelist'>Home</Link></li> 
+          <li className='navlist'>
+            <Link to="/" className='activelist'>Home</Link>
+          </li>
           <li className='navlist'>Online Yoga</li>
           <li className='navlist'>
             <Link to='/services' className='activelist'>Services</Link>
           </li>
           <li className='navlist'>Healing Stories</li>
-          <li className='navlist'><Link to="/sign-in" className='activelist'>SignIn</Link></li>
+          {/* Conditionally render or disable SignIn */}
+          {!isLoggedIn && (
+            <li className='navlist'>
+              <Link to="/sign-in" className='activelist'>SignIn</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li className='navlist'>
+              <button className="btn22" disabled>Already Signed In</button>
+            </li>
+          )}
           <li>
             <Link className='adjust' to="/book-appointment">
               <button className="btn22">Book an Appointment</button>
-            </Link> 
+            </Link>
           </li>
         </ul>
       </div>
