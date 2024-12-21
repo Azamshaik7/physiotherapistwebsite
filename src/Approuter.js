51a8e56717be2ecb225a93842e602dfa1575d822
Azamshaik7 <azamshari68@gmail.com>
2024-12-21 20:13:15 +0530

    aa

--- src/Approuter.js
+++ src/Approuter.js
@@ -20,7 +20,7 @@ const ProtectedRoute = ({ element: Component }) => {
 
 export default function Approuter() {
   return (
-    <Router basename="/physiotherapistwebsit">
+    <Router basename="/physiotherapistwebsite">
 
       <Routes>
         <Route path="/" element={<ProtectedRoute element={<Home />} />} />
